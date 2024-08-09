a272210c0dc330a9ffcc9a87701fa6ba71d8ff53
Bhadra Mohit <158451254+BhadraMohit09@users.noreply.github.com>
2024-08-09 19:42:38 +0530

    FS and PATH Modules in NodeJS

---
+++ index.js
@@ -0,0 +1,67 @@
+// const path = require('path');
+
+// const DataA = path.dirname("C:/FolderA/FolderB/FileA.html");
+// const DataB = path.basename("C:/FolderA/Folder/FileA.html");
+// const Datac = path.extname("C:/FolderA/Folder/FileA.html");
+// const DataD = path.parse("C:/FolderA/Folder/FileA.html");
+// const DataE = path.isAbsolute("C:/FolderA/Folder/FileA.html");
+// const changeFormat = {
+
+// }
+// path.format(changeFormat);
+
+// path.join('','');
+
+// path.normalize('');
+
+// path.relative('', '');
+
+// path.resolve('');
+
+// console.log(DataA);
+
+// console.log(DataB);
+
+// console.log(Datac);
+
+// console.log(DataE);
+
+// console.log(DataD);
+
+// const fs = require('fs');
+
+// fs.writeFileSync('MyFileA.txt', 'This is my file');
+
+// const ReadData = fs.readFileSync('MyFileA.txt', 'utf-8');
+
+// fs.appendFileSync('NewFile.txt', 'Appended Text');
+
+// console.log(ReadData);
+
+// fs.renameSync('MyFileA.txt', 'NewFile.txt'); 
+
+// fs.unlinkSync('NewFile.txt');
+
+// fs.writeFile('AsyncFile.txt', 'New file created', (e)=>{
+//     if(e){
+//         console.log(e);
+//     }else{
+//         console.log("File Created Successfily");
+//     }
+// });
+
+// fs.readFile('AsyncFile.txt', 'utf-8', (e, ReadData)=>{
+//     if(e){
+//         console.log(e);
+//     }
+//     else{
+//         console.log(ReadData);
+//     }
+// });
+
+// fs.rename('', '');
+
+// fs.appendFile('');
+
+// fs.unlink('')
+    
