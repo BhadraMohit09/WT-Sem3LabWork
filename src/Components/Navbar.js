a75c0b5d3c6e528494d433714952ba95a09856f8
Bhadra Mohit <mohitbhadra18@gmail.com>
2024-09-18 18:34:43 +0530

    ReactJS Routing Using Outlet@MAB

---
+++ src/Components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom"
+const Navbar = () => {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <Link class="navbar-brand">Navbar</Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <Link to='/home' class="nav-link active" aria-current="page">GetAll</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" to='/contact'>Contact</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link">Pricing</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link disabled" aria-disabled="true">Disabled</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Layout from './Components/Layout';
+import GetByID from './Components/GetByID';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+  
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='/home' element={<About />}/>
+        <Route path='/contact' element={<Contact />} />
+        <Route path='home/:id' element={<GetByID />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
