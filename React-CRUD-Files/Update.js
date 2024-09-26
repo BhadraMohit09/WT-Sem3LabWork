2802e3508bf22d042e83720dab3fe3ed5bd69cde
Bhadra Mohit <mohitbhadra18@gmail.com>
2024-09-26 17:11:49 +0530

    ReactJS CRUD Files

--- React-CRUD-Files/Update.js
+++ React-CRUD-Files/Update.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+// import { useState, useEffect } from "react";
+// import { useNavigate, useParams } from "react-router-dom";
 
 // const Update = () => {
 //   const [data, setData] = useState({});
@@ -64,66 +64,3 @@ import { useNavigate, useParams } from "react-router-dom";
 // export default Update;
 
 
-const Update = ()=>{
-    const [data, setData] = useState({});
-
-    const {id} = useParams();
-
-    const navigate = useNavigate();
-
-    useEffect(()=>{
-        fetch(`http://localhost:7000/home/${id}`)
-        .then((res)=>res.json())
-        .then((res)=>setData(res))
-    },[])
-
-    return(
-
-        <>
-            <div>
-                <input
-                disabled
-                type="text"
-                value={data.id}
-                onChange={(e)=>{
-                    setData({...data, id: e.target.value})
-                }}
-                />
-
-                <input 
-                type="text"
-                value={data.name}
-                onChange={(e)=>{
-                    setData({...data, name: e.target.value})
-                }}
-                />
-
-                <input 
-                value={data.phone}
-                type="text"
-                onChange={(e)=>{
-                    setData({...data, phone: e.target.value})
-                }}
-                />
-
-                <button 
-                className="btn btn-warning"
-                onClick={()=>{
-                    fetch('http://localhost:7000/home/update/'+ id, {
-                        method: 'PATCH',
-                        body: JSON.stringify(data),
-                        headers:{
-                            "Content-Type":"application/json"
-                        }
-                    })
-                    .then((res) => res.json())
-                    .then(() => navigate('/home'))
-                }}
-                >Edit Data</button>
-
-            </div>
-        </>
-    )
-}
-
-export default  Update;
